321a510d6e526881cbf98ffe48ae2d89a9e8bd83
Gilberto <38070920+gilberto-009199@users.noreply.github.com>
2021-05-01 18:27:03 -0300

    Update tabuleiro.py

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Números Primos felizes
 JOGADOR_BOLINHA = 7;
 JOGADOR_XIZINHO = 13;
@@ -11,9 +12,12 @@ class Tabuleiro:
         self.estado = [[0,0,0],
                        [0,0,0],
                        [0,0,0]];
+        self.lances = 0;
+        self.ganhador = "";
     def posicaoValida(self, x, y, jogador):
         valido = False;
-
+        #print("x:", x ,", y:",y,"jogador:",jogador);
+        #print(self.estado);
         # verifica se o Simbolo passado e valido
         valido = jogador == JOGADOR_BOLINHA or jogador == JOGADOR_XIZINHO;
         # Verifica se a posição an matriz e valida
@@ -24,8 +28,10 @@ class Tabuleiro:
         return valido;
 
     def colocarPeca(self, x, y, jogador):
-        
-        self.estado[x][y] == jogador;
+        self.estado[x][y] = jogador;
+        self.lances += 1;
+        if (self.existeVencedor()):
+            print(self.ganhador)
 
     def imprimir(self):
         retorno = "";
@@ -36,6 +42,9 @@ class Tabuleiro:
         return retorno;
 
     def existeVencedor(self):
+        if self.lances > 9:
+            self.ganhador = ("ocorreu um empate!");
+            return True;    
         existe = False;
         for peca in self.pecas:
             # Bloqueia a execução do 0
@@ -47,28 +56,31 @@ class Tabuleiro:
                 for linha in self.estado:
                     soma = linha[0]+linha[1]+ linha[2];
                     if soma == peca * 3:
-                        print("Temos um vencedor: " + self.pecas[peca]);
-                        return True
+                        self.ganhador = "Temos um vencedor: " + self.pecas[peca];
+                        return True;
                 # calcula coluna                X O
                 #                               X O
                 #                               X O
                 for coluna in range(0, 3):
                     soma = self.estado[0][coluna] + self.estado[1][coluna] + self.estado[2][coluna];
                     if soma == peca * 3:
-                        print("Temos um vencedor: " + self.pecas[peca]);
-                        return True
+                        self.ganhador = "Temos um vencedor: " + self.pecas[peca];
+                        return True;
                 # calcula vertical              X O O
                 #                               O X O
                 #                               O O X
                 soma = self.estado[0][0] + self.estado[1][1] + self.estado[2][2];
                 if soma == peca * 3:
-                        print("Temos um vencedor: " + self.pecas[peca]);
-                        return True
+                        self.ganhador = ("Temos um vencedor: " + self.pecas[peca]);
+                        return True;
                 # calcula vertical Direita      X X O
                 #                               X O X
                 #                               O X X
                 soma = self.estado[0][2] + self.estado[1][1] + self.estado[2][0];
                 if soma == peca * 3:
-                    print("Temos um vencedor: " + self.pecas[peca]);
+                    self.ganhador = ("Temos um vencedor: " + self.pecas[peca]);
                     return True;
+        if self.lances > 8:
+            self.ganhador = ("ocorreu um empate!");
+            return True;
         return existe;
